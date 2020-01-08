a00076f15b6a734c6114596c99ef7cc2778b89b2
OKANO Kazumi <kazumi.okano@morson.jp>
2020-01-08 17:00:17 +0900

    クラス内Enum

--- CLRApp/CLRApp.cpp
+++ CLRApp/CLRApp.cpp
@@ -3,18 +3,20 @@
 using namespace System;
 using namespace CsLibrary;
 
-int getValue(CsLibrary::CsEnum^ csEnum)
+int getValue( CsLibrary::CsEnum ^ csEnum )
 {
 	return static_cast<int>( *csEnum );
 }
 
-int main(array<System::String ^> ^args)
+int main( array<System::String ^> ^ args )
 {
 	Console::WriteLine( CsLibrary::CsEnum::Zero );
 	Console::WriteLine( CsLibrary::CsEnum::One );
 	Console::WriteLine( CsLibrary::CsEnum::Two );
 
-	Console::WriteLine( getValue(CsLibrary::CsEnum::Two) );
+	Console::WriteLine( getValue( CsLibrary::CsEnum::Two ) );
+
+	Console::WriteLine( CsLibrary::CsClass::CsClassEnum::ClassTwo );
 
 	return 0;
 }
--- CsLibrary/EnumShare.cs
+++ CsLibrary/EnumShare.cs
@@ -2,8 +2,22 @@
 {
 	public enum CsEnum
 	{
-		Zero= 0,
+		Zero = 0,
 		One,
 		Two,
+	};
+
+	public class CsClass
+	{
+		public CsClass()
+		{
+		}
+
+		public enum CsClassEnum
+		{
+			ClassZero = 0,
+			ClassOne,
+			ClassTwo,
+		};
 	};
 }

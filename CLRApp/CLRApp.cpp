6a2b5c746b5503be56820f97ec854094d9a5154d
OKANO Kazumi <kazumi.okano@morson.jp>
2020-01-17 14:21:45 +0900

    ^でのenum引き渡し。

--- CLRApp/CLRApp.cpp
+++ CLRApp/CLRApp.cpp
@@ -8,6 +8,15 @@ int getValue( CsLibrary::CsEnum ^ csEnum )
 	return static_cast<int>( *csEnum );
 }
 
+class EnumTest {
+public:
+	CsLibrary::CsEnum ^ GetEnum() {
+		CsLibrary::CsEnum ^ res = gcnew CsLibrary::CsEnum;
+		res						= CsLibrary::CsEnum::Two;
+		return res;
+	}
+};
+
 int main( array<System::String ^> ^ args )
 {
 	Console::WriteLine( CsLibrary::CsEnum::Zero );
@@ -18,5 +27,11 @@ int main( array<System::String ^> ^ args )
 
 	Console::WriteLine( CsLibrary::CsClass::CsClassEnum::ClassTwo );
 
+	EnumTest test;
+	CsLibrary::CsEnum^ res =  test.GetEnum();
+
+	Console::WriteLine( res );
+
+	getchar();
 	return 0;
 }
